a3e84e7ffddd54faf2a42aafe82c336704656208
Thibault Francois <francois.th@gmail.com>
2016-10-16 13:18:14 +0200

    [ADD] personnal asset management, warranty and invoice storing

---
+++ perso_account_asset/models/asset.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+'''
+Created on 15 oct. 2016
+
+@author: mythrys
+'''
+
+from openerp import models, fields, api, _
+from openerp.exceptions import ValidationError
+
+class AssetDocument(models.Model):
+    _name = "perso.account.asset_document"
+
+    name = fields.Char("Document Reason")
+    fname = fields.Char("Filename")
+    data = fields.Binary("File")
+    asset_id = fields.Many2one("perso.account.asset" ,"Asset")
+
+class Asset(models.Model):
+    _name = "perso.account.asset"
+
+    name = fields.Char(required=True)
+    cash_flow_id = fields.Many2one('perso.account.cash_flow', string="Move")
+    start_date = fields.Date('Buy date')
+    end_date = fields.Date('End of Warranty')
+    value = fields.Float()
+    document_ids = fields.One2many('perso.account.asset_document', 'asset_id')
+
